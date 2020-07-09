fa6a0cecbe2c2a5c1187947cc8e0034e106ef7c0
Marie Foxall <marie@wastedeffort.ca>
2020-07-09 17:11:58 -0400

    Stretch Goals

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const handleHomepage = (req, res) => {
 };
 
 const handleProfilePage = (req, res) => {
+  console.log(currentUser);
   const currentProfile = users.find((user) => {
     return user._id === req.params.id;
   });
@@ -62,6 +63,76 @@ const handleName = (req, res) => {
   }
 };
 
+const handleRemove = (req, res) => {
+  const unfriendID = req.body.removeID.toString();
+  const unfriendIndex = currentUser.friends.findIndex((friendID) => {
+    return friendID === unfriendID;
+  });
+  currentUser.friends.splice(unfriendIndex, 1);
+  const unfriended = users.find((user) => {
+    return user._id === unfriendID;
+  });
+  const currentuserIndex = unfriended.friends.findIndex((friendID) => {
+    return friendID === currentUser._id;
+  });
+  unfriended.friends.splice(currentuserIndex, 1);
+
+  console.log(currentUser.friends);
+  res.status(200).redirect(`/users/${unfriendID}`);
+};
+
+const handleAddFriend = (req, res) => {
+  const newfriendID = req.body.addID.toString();
+  const newFriend = users.find((user) => {
+    return user._id === newfriendID;
+  });
+  newFriend.pendingfriends = [];
+  newFriend.pendingfriends.push(currentUser._id);
+  res.status(200).redirect(`users/${newfriendID}`);
+};
+
+const handleAccept = (req, res) => {
+  const acceptFriendID = req.body.acceptfriendID.toString();
+  currentUser.friends.push(acceptFriendID);
+  const acceptFriend = users.find((user) => {
+    return user._id === acceptFriendID;
+  });
+  acceptFriend.friends.push(currentUser._id);
+  const acceptFriendIndex = currentUser.friends.findIndex((friendID) => {
+    return friendID === acceptFriendID;
+  });
+  currentUser.pendingfriends.splice(acceptFriendIndex, 1);
+  res.status(200).redirect(`users/${acceptFriendID}`);
+};
+
+const handleSignout = (req, res) => {
+  currentUser = {};
+  res.status(200).redirect("/signin");
+};
+
+const handleSignUp = (req, res) => {
+  res.status(200).render("pages/signup", {
+    users: users,
+    currentUser: currentUser,
+  });
+};
+
+const handleNewUser = (req, res) => {
+  const newUserName = req.body.newUserFirstName;
+  console.log("line 122", req.body);
+  const newUserImage = req.body.newUserImg;
+  users.push({
+    _id: `${1010 + users.length}`,
+    name: newUserName,
+    friends: [],
+    avatarUrl: newUserImage,
+  });
+  res.status(200).render("pages/homepage", {
+    users: users,
+    currentUser: currentUser,
+  });
+  // console.log(users);
+};
 // -----------------------------------------------------
 // server endpoints
 express()
@@ -78,7 +149,19 @@ express()
 
   .get("/signin", handleSignin)
 
+  .get("/signup", handleSignUp)
+
   .post("/getname", handleName)
+
+  .post("/removefriend", handleRemove)
+
+  .post("/addfriend", handleAddFriend)
+
+  .post("/acceptfriend", handleAccept)
+
+  .post("/usersignup", handleNewUser)
+
+  .post("/signout", handleSignout)
 
   // a catchall endpoint that will send the 404 message.
   .get("*", handleFourOhFour)
